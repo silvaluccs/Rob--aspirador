0d0dd8ba08cc9e80f238d5cf1a18d8a63bc13537
Lucas <163942131+silvaluccs@users.noreply.github.com>
2024-09-08 12:33:40 -0300

    Integração da bateria e refatoração do código.

---
+++ problema 1/segB.v
@@ -0,0 +1,16 @@
+/*
+Modulo responsavel por controlar o acionamento do segmento B
+das matrizes de leds.
+*/
+
+module segB(out, SenE, SenF, SenD, Control);
+	input SenE, SenF, SenD, Control;
+	output out;
+	wire notD, or1, and1;
+	
+	not NotD(notD, SenD);
+	or Or1(or1, notD, SenE);
+	and And1(and1, or1, SenF);
+	nand Nand1(out, and1, Control);
+	
+endmodule
